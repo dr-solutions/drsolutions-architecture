80edd507b0d621b73a376862e062056e85c59fbc
Jakob <office@drsolutions.at>
2018-03-10 18:08:10 +0100

    timezone fuer db-connection

--- src/main/java/at/drsolutions/dao/TerminDAO.java
+++ src/main/java/at/drsolutions/dao/TerminDAO.java
@@ -7,8 +7,7 @@ import at.drsolutions.persistence.Termin;
 public class TerminDAO extends ServiceDAO {
 
 	public List<Termin> getAllTermine() {
-		throw new RuntimeException("Exception beim Lesen der Termine :(");
-		// return getEntityManager().createNamedQuery(Termin.FIND_ALL,
-		// Termin.class).getResultList();
+		// throw new RuntimeException("Exception beim Lesen der Termine :(");
+		return getEntityManager().createNamedQuery(Termin.FIND_ALL, Termin.class).getResultList();
 	}
 }
